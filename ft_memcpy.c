794d5a72ef9cdb4906cdbae677c8f0e58b68fe6b
Even Zeppa <zeppaeven.dev@gmail.com>
2024-11-06 11:17:19 +0100

    ADD main.c, ft_memcmp.c and ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ezeppa <ezeppa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 11:51:25 by ezeppa            #+#    #+#             */
+/*   Updated: 2024/11/06 11:16:29 by ezeppa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*ptr_dest;
+	unsigned char	*ptr_src;
+	size_t			i;
+
+	ptr_dest = (unsigned char *)dest;
+	ptr_src = (unsigned char *)src;
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	while (i < n)
+		ptr_dest[i] = ptr_src[i++];
+	return (dest);
+}
---
+++ main.c
@@ -0,0 +1,23 @@
+#include <string.h>
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+int	main(int argc, char *argv[])
+{
+	char	*text;
+	char	*key;
+	int		gap;
+	int		is_vigenere;
+	int		is_cesar;
+	int		i;
+
+	is_vigenere = 0;
+	is_cesar = 0;
+	i = 1;
+	while (i < argc)
+	{
+		i++;
+	}
+	return (0);
+}
